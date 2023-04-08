2ccf4c68831d1869dfdd60e9f42de2a168b79c9c
Arya Vijayan <aryavijayanrohini@gmail.com>
2023-04-08 11:27:34 +0530

    save uploaded file to uploads folder and login button

--- app.py
+++ app.py
@@ -1,10 +1,33 @@
-from flask import Flask, render_template
+from flask import Flask, render_template,request, redirect, url_for
+import os
+from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
 @app.route('/', methods=['GET'])
 def hello(name=None):
     return render_template('index.html')
 
+@app.route('/predict', methods=['GET', 'POST'])
+def upload():
+    if request.method == 'POST':
+        # Get the file from post request
+        f = request.files['file']
+
+        # Save the file to ./uploads
+        basepath = os.path.dirname(__file__)
+        file_path = os.path.join(
+            basepath, 'uploads', secure_filename(f.filename))
+        f.save(file_path)
+        
+    #add processing code
+
+
+
+
+    
+        text = "Prediction : "
+
+        return text
 
 if __name__ =='__main__':
 	#app.debug = True
